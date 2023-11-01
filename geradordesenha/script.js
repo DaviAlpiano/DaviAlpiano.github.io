bba4e3b65dbffd65bce14fd26587c3021a0d5049
Davi Alpiano <davialpiano@gmail.com>
2023-11-01 19:31:23 -0300

    htmlJs

--- geradordesenha/script.js
+++ geradordesenha/script.js
@@ -12,7 +12,9 @@ const gerarsenha = (tamanho) => {
   return passorwd
 } 
 
-const gerarsenhas = (tamanho) => {
+function gerarsenhas(){
+    let tamanho = document.getElementById('qNumber').value
+    
 
     const cara = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
     'A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','@','#','$','%','^','&','*','(',')','+','?','>','<',':','{','}','[',']']
@@ -23,5 +25,5 @@ const gerarsenhas = (tamanho) => {
     let ramdomnumb = Math.floor(Math.random() * cara.length);
     passorwd += cara[ramdomnumb]
     }
-   return passorwd
+   document.getElementById('resposta').innerText = passorwd;
 } 
